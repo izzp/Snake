b18774651e04455c4b63a2ddd8e796be02cc7693
e <i@mezzp.com>
2018-11-26 17:40:53 +0800

    添加菜单

--- Snake/Form1.Designer.cs
+++ Snake/Form1.Designer.cs
@@ -32,21 +32,29 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.btnRun = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.开始游戏ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.游戏设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.帮助ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.关于ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.panel1.Location = new System.Drawing.Point(12, 25);
+            this.panel1.Location = new System.Drawing.Point(16, 31);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(600, 480);
+            this.panel1.Size = new System.Drawing.Size(799, 599);
             this.panel1.TabIndex = 0;
             // 
             // btnRun
             // 
-            this.btnRun.Location = new System.Drawing.Point(659, 65);
+            this.btnRun.Location = new System.Drawing.Point(879, 81);
+            this.btnRun.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnRun.Name = "btnRun";
-            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.Size = new System.Drawing.Size(100, 29);
             this.btnRun.TabIndex = 1;
             this.btnRun.Text = "开始";
             this.btnRun.UseVisualStyleBackColor = true;
@@ -57,18 +65,62 @@
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.开始游戏ToolStripMenuItem,
+            this.游戏设置ToolStripMenuItem,
+            this.帮助ToolStripMenuItem,
+            this.关于ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1037, 28);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 开始游戏ToolStripMenuItem
+            // 
+            this.开始游戏ToolStripMenuItem.Name = "开始游戏ToolStripMenuItem";
+            this.开始游戏ToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.开始游戏ToolStripMenuItem.Text = "开始游戏";
+            // 
+            // 游戏设置ToolStripMenuItem
+            // 
+            this.游戏设置ToolStripMenuItem.Name = "游戏设置ToolStripMenuItem";
+            this.游戏设置ToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.游戏设置ToolStripMenuItem.Text = "游戏设置";
+            // 
+            // 帮助ToolStripMenuItem
+            // 
+            this.帮助ToolStripMenuItem.Name = "帮助ToolStripMenuItem";
+            this.帮助ToolStripMenuItem.Size = new System.Drawing.Size(51, 24);
+            this.帮助ToolStripMenuItem.Text = "帮助";
+            // 
+            // 关于ToolStripMenuItem
+            // 
+            this.关于ToolStripMenuItem.Name = "关于ToolStripMenuItem";
+            this.关于ToolStripMenuItem.Size = new System.Drawing.Size(51, 24);
+            this.关于ToolStripMenuItem.Text = "关于";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(778, 517);
+            this.ClientSize = new System.Drawing.Size(1037, 646);
             this.Controls.Add(this.btnRun);
             this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -77,6 +129,11 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button btnRun;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 开始游戏ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 游戏设置ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 帮助ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 关于ToolStripMenuItem;
     }
 }
 
